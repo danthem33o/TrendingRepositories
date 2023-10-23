0adb9a60af881bc7ad7208282b9501f1aa7d4e81
Daniel Hutchinson <daniel.hutchinson@salecycle.com>
2023-10-23 06:02:25 +0100

    Added helper text to the favourite section

--- src/pages/HomePage/components/FavouritesSection.tsx
+++ src/pages/HomePage/components/FavouritesSection.tsx
@@ -1,7 +1,8 @@
-import { Typography } from "@mui/material";
+import { Icon, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { RepositoryInfoCard } from "../../../components/RepositoryInfoCard";
 import { useTrendingRepositories } from "../../../state/TrendingRepositories/hooks/useTrendingRepositories";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 export const FavouritesSection = () => {
   const { favourites } = useTrendingRepositories();
@@ -11,27 +12,37 @@ export const FavouritesSection = () => {
       <Typography variant="h5" sx={{ width: "100%" }}>
         Favourited repositories ({favourites.length})
       </Typography>
-      <Box
-        sx={{
-          overflowX: "auto",
-          display: "flex",
-          gap: 2,
-          flexDirection: "row",
-          padding: "10px 5px",
-        }}
-      >
-        {favourites.map((s) => (
-          <RepositoryInfoCard
-            key={s.id}
-            id={s.id}
-            name={s.name}
-            ownerName={s.owner.name}
-            githubLink={s.url}
-            numberOfStars={s.stars}
-            description={s.description}
-          />
-        ))}
-      </Box>
+      {favourites.length ? (
+        <Box
+          sx={{
+            overflowX: "auto",
+            display: "flex",
+            gap: 2,
+            flexDirection: "row",
+            padding: "10px 5px",
+          }}
+        >
+          {favourites.map((s) => (
+            <RepositoryInfoCard
+              key={s.id}
+              id={s.id}
+              name={s.name}
+              ownerName={s.owner.name}
+              githubLink={s.url}
+              numberOfStars={s.stars}
+              description={s.description}
+            />
+          ))}
+        </Box>
+      ) : (
+        <div style={{}}>
+          Nothing favourited yet! Click the{" "}
+          <Icon>
+            <FavoriteBorderIcon />
+          </Icon>{" "}
+          to start favouritting
+        </div>
+      )}
     </>
   );
 };
